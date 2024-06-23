8160d2fef991b8b626f533593c25824d60965119
flyingperson23 <flyingperson23@gmail.com>
2024-06-23 19:51:14 -0400

    logo+actual mod class

---
+++ src/main/java/gcg/GCG.java
@@ -0,0 +1,23 @@
+package gcg;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "gcg", name = "Galacticraft Gravity", version = "1.0", dependencies = "required: galacticraftcore")
+public class GCG {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e){
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e){
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e){
+    }
+}
+
